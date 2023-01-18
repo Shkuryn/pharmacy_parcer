efb5ee8637f07949a289cf72166ce3600a8e3c60
igor shkuryn <shkuryn@gmail.com>
2023-01-19 00:42:42 +0400

    amount_with_discount

--- index.rb
+++ index.rb
@@ -14,9 +14,12 @@ product_elements.each do |product|
 
     product_title = details_text.at_css('.product-title')
     amount = details_text.at_css('.amount')
-    country  = details_text.css('div strong')[0].content
+    amount_with_discount = details_text.css('ins .amount').text.gsub(/[^\d.]/, '')
+
+    country  = details_text.css('div strong')[0]
     puts "title= #{product_title}"
     puts "amount = #{amount}"
+    puts "amount_with_discount = #{amount_with_discount}"
     puts country
 
   end
