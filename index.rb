01d8b7252b5a10a6ba4cf09fd042a1742592a238
igor shkuryn <shkuryn@gmail.com>
2023-01-19 00:07:01 +0400

    first draft

---
+++ index.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'open-uri'
+require 'nokogiri'
+
+doc = Nokogiri::HTML(URI.open('https://www.aversi.ge/en/aversi/act/searchMedicine/?kw_en=Adazal'))
+product_elements  = doc.css('.product')
+
+product_elements.each do |product|
+  product_details = product.at('.product-details').children.map(&:to_html)
+  product_details = product_details.reject { |e| e == "\n" }
+  product_details.each do |details|
+    details_text = Nokogiri::HTML(details)
+
+    product_title = details_text.at_css('.product-title')
+    amount = details_text.at_css('.amount')
+    country  = details_text.css('div strong')[0].content
+    puts "title= #{product_title}"
+    puts "amount = #{amount}"
+    puts country
+
+  end
+
+end
+
