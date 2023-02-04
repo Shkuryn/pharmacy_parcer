7848fc0af5987f4e0ea52e333ca2acf1ff33d03b
igor shkuryn <shkuryn@gmail.com>
2023-02-05 00:02:39 +0400

    add translation

--- psp_searcher.rb
+++ psp_searcher.rb
@@ -1,2 +1,36 @@
-class PSP_searcher
+# frozen_string_literal: true
+
+require 'open-uri'
+require 'nokogiri'
+require 'selenium-webdriver'
+require 'uri'
+require 'net/http'
+require 'openssl'
+require './drug'
+require './translate'
+
+# PspSearcher class performs a search for a given drug name on a specified website
+# using Selenium and outputs the resulting elements
+class PspSearcher
+  def initialize(drug_name)
+    @drug_name = drug_name
+  end
+
+  def search
+    url = "https://psp.ge/catalogsearch/result?q=#{@drug_name}"
+    driver = Selenium::WebDriver.for :chrome
+    driver.navigate.to url
+
+    elements = driver.find_elements(:class, 'product')
+
+    elements.each do |element|
+      puts "перевожу #{element.text}"
+      gt = GoogleTranslator.new(element.text)
+      gt.translate
+      puts "перевод #{gt.text_to}"
+    end
+
+    driver.quit
+  end
+
 end
--- translate.rb
+++ translate.rb
@@ -2,13 +2,30 @@
 
 require 'google/cloud/translate/v2'
 require 'dotenv'
-Dotenv.load('./.env')
+Dotenv.load('.env')
 
-API_KEY = ENV['API_KEY']
-translate = Google::Cloud::Translate::V2.new(
-  key: API_KEY
-)
-text = 'გამარჯობა'
-translation = translate.translate text, to: 'ru'
-puts translation.text
+# GoogleTranslator is a Ruby class for translating text using Google Cloud Translate API.
+#
+#   Class Variables
+#  API_KEY - The API key for accessing Google Cloud Translate API. The value should be set from the ENV variable.
+#    Attributes
+# text_from - The text to be translated. It is initialized when a new instance of the class is created.
+# text_to - The translated text. It is set after calling the translate method.
+#    Methods
+# initialize(text_from) - The constructor method for the class. It takes the text_from argument and sets it as an
+# instance variable.
+# translate - Translates the text from text_from to Russian ('ru') and sets the result as text_to.
+# This method uses the Google Cloud Translate API and requires the API_KEY to be set.
+class GoogleTranslator
+  API_KEY = ENV['API_KEY']
+  attr_reader :text_from, :text_to
 
+  def initialize(text_from)
+    @text_from = text_from
+  end
+
+  def translate
+    translate = Google::Cloud::Translate::V2.new(key: API_KEY)
+    @text_to = translate.translate(@text_from, to: 'ru')
+  end
+end
